33a0c445b0e1d34ff73ceb5e31c5804f354e3c42
eeexuanlyx <eeexuanlyx@gmail.com>
2024-11-30 00:11:49 +0800

    update env

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "db_user",
-  password: "example",
-  host: "localhost",
-  port: 5432,
-  database: "cakeparcel",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
